21f4033f16c773b8480e84c7a1df49f475e8311d
dtc105 <dtcooper105@gmail.com>
2024-12-12 01:12:14 -0500

    added icons and more to about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,11 +10,12 @@ function About() {
                 The way this works is you enter in a complex valued equation with variables x, y, z, and c, and the program will iterate the function <code>z<sub>n+1</sub> = f(z<sub>n</sub>)</code>.<br/>
                 z is your complex number valued at x + iy,<br/>
                 x is the real part of z and y is the imaginary part of z. <br/>
-                c is the complex number evaluated where you started at, unlike z it's value remains the same.
+                c is the complex number evaluated where you started at, unlike z it's value remains the same.<br/>
+                Try typing <code>z<sup>2</sup>+c</code> to see the <a target="_blank" href="https://en.wikipedia.org/wiki/Mandelbrot_set" className="underline text-blue-800">Mandelbrot set</a>
             </section>
             <section  className="w-fit">
-                Code can be found here: <a href="https://github.com/dtc105/Fractal-Drawer" className="underline text-blue-800">https://github.com/dtc105/Fractal-Drawer</a><br/>
-                I can be found here: <a href="https://www.linkedin.com/in/dcooper105">https://www.linkedin.com/in/dcooper105</a><br/>
+                Code can be found here: <a target="_blank" href="https://github.com/dtc105/Fractal-Drawer" className="underline text-blue-800">https://github.com/dtc105/Fractal-Drawer</a><br/>
+                I can be found here: <a target="_blank" href="https://www.linkedin.com/in/dcooper105" className="underline text-blue-800">https://www.linkedin.com/in/dcooper105</a><br/>
             </section>
 
             <p>Enjoy!</p>
